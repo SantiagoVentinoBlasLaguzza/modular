b2abc8c2ab35d5edf8290009d868cbe74787fda5
Santiago Valentino Blas Laguzza <sblas@imal.unl.edu.ar>
2025-06-07 16:31:05 -0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# fmri_features/__init__.py
+# Este archivo puede estar vacío.
+# Su presencia convierte al directorio 'qc_bold' en un paquete de Python,
+# permitiendo importaciones relativas como `from . import io`.
